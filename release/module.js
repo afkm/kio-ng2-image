12c706919ad2a5424227215360e154f28ab7292a
Matthias Buchholz <matthias@afkm.io>
2017-09-22 23:49:07 +0200

    i18n module

--- release/module.js
+++ release/module.js
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { KioNg2ComponentRoutingModule } from 'kio-ng2-component-routing';
 import { KioCtnModule } from 'kio-ng2-ctn';
 import { ImageComponent } from './components/image/image.component';
+import { KioNg2i18nModule } from 'kio-ng2-i18n';
 var KioNg2ImageModule = /** @class */ (function () {
     function KioNg2ImageModule() {
     }
     KioNg2ImageModule.decorators = [
         { type: NgModule, args: [{
-                    imports: [KioNg2ComponentRoutingModule, KioCtnModule],
+                    imports: [KioNg2ComponentRoutingModule, KioCtnModule, KioNg2i18nModule],
                     declarations: [ImageComponent],
                     entryComponents: [ImageComponent],
-                    exports: [KioNg2ComponentRoutingModule, ImageComponent, KioCtnModule]
+                    exports: [KioNg2ComponentRoutingModule, ImageComponent, KioCtnModule, KioNg2i18nModule]
                 },] },
     ];
     /** @nocollapse */
--- src/module.ts
+++ src/module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core'
 import { KioNg2ComponentRoutingModule } from 'kio-ng2-component-routing'
 import { KioCtnModule } from 'kio-ng2-ctn'
 import { ImageComponent } from './components/image/image.component'
+import { KioNg2i18nModule } from 'kio-ng2-i18n'
+
 
 @NgModule({
-  imports: [ KioNg2ComponentRoutingModule, KioCtnModule ],
+  imports: [ KioNg2ComponentRoutingModule, KioCtnModule, KioNg2i18nModule ],
   declarations: [ ImageComponent ],
   entryComponents: [ ImageComponent ],
-  exports: [ KioNg2ComponentRoutingModule, ImageComponent, KioCtnModule ]
+  exports: [ KioNg2ComponentRoutingModule, ImageComponent, KioCtnModule, KioNg2i18nModule ]
 })
 export class KioNg2ImageModule {}
